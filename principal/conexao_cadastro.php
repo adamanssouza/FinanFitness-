cf232d5764d12c76486c269d937905045a4c9fc0
Kauê Lins <kauelinnss@gmail.com>
2025-04-10 20:48:27 -0300

    mensagem da mudança

--- principal/conexao_cadastro.php
+++ principal/conexao_cadastro.php
@@ -1,7 +1,7 @@
 <?php
     define('HOST', 'localhost');
     define('USER', 'root');
-    define('PASS', 'kklidi12');
+    define('PASS', '');
     define('BASE', 'cadastro_finan');
 
     $conn = new mysqli(HOST, USER, PASS, BASE);
